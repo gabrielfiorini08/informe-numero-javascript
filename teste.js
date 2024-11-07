31e6ca4af52deb4fa17c56840efa54a29937ae5b
gabrielfiorini08 <165048770+gabrielfiorini08@users.noreply.github.com>
2024-11-07 09:51:58 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("Informe o primeiro número: "));
+let numero2 = parseInt(prompt("Informe o segundo número: "));
+let soma = numero1 + numero2;
+
+document.write("A soma dos números digitados é ", soma);
+
+
+
